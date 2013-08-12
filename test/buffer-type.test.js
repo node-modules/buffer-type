06155189a5cd66e288d8e3774610c92ba456376a
fengmk2 <fengmk2@gmail.com>
2013-08-12 18:07:14 +0800

    first commit

---
+++ test/buffer-type.test.js
@@ -0,0 +1,18 @@
+/*!
+ * buffer-type - test/buffer-type.test.js
+ * Copyright(c) 2013 fengmk2 <fengmk2@gmail.com>
+ * MIT Licensed
+ */
+
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var buffer-type = require('../');
+var should = require('should');
+
+describe('buffer-type.test.js', function () {
+  
+});
